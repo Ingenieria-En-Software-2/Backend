b0b1c3f77a00ba37038e22353558dc4a200bdb1f
admeneses <alejandrocardie12@gmail.com>
2023-05-29 23:49:06 -0400

    anadido carga de variables del .env

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()  # Carga las variables de entorno del archivo .env
 
 
 class DevConfig():
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://localhost:5432/caribbeanWalletdb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BUNDLE_ERRORS = True
