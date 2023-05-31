1f316c07fe92a918c56a8dacc3915c180de8c9af
admeneses <alejandrocardie12@gmail.com>
2023-05-31 01:07:44 -0400

    removed default value of sqlalchemy_database_uri

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ from os import environ
 
 class DevConfig:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://localhost:5432/caribbeanWalletdb')
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BUNDLE_ERRORS = True
