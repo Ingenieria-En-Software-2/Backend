60a53f7b6b0e7b29bc6e42979cad8981c87c59d3
luiss-23 <lblancog23@gmail.com>
2023-06-04 17:41:29 -0400

    fixing imports

--- app/webapp/auth/controllers.py
+++ app/webapp/auth/controllers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, make_response, jsonify
 from flask.views import MethodView
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 
 
 from .. import db, bcrypt
