5f5a9b175573202fec780136232cb9de0b6746a4
Adelina Figueira <51302331+adelinarf@users.noreply.github.com>
2023-07-10 15:42:27 -0400

    Update controllers.py

--- app/webapp/api/user_transactions/controllers.py
+++ app/webapp/api/user_transactions/controllers.py
@@ -5,30 +5,34 @@ CrudApi class, and is in charge of handling HTTP requests related to account hol
 
 from flask import abort, request
 from flask_restful import fields
-from ...auth.models import db, User
+from webapp.auth.models import db, User
 from .models import UserTransaction
 from .UserTransactionsRepository import UserTransactionsRepository
-from ...api.generic.CrudApi import CrudApi
+from webapp.api.generic.CrudApi import CrudApi
 from .schemas import Get_User_Transaction_Schema
 
 # Instance of the account holder repository
 user_transactions_repository = UserTransactionsRepository(db)
 import datetime
 
-from marshmallow import ValidationError
-
-
-from ...api.user_account.UserAccountRepository import UserAccountRepository
-
-
-from ..user_account.schemas import (
-    Create_User_Account_Schema,
-    Update_User_Account_Schema,
-    Get_User_Account_Schema,
+from marshmallow import (
+    ValidationError
 )
 
 
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from webapp.api.user_account.UserAccountRepository import UserAccountRepository
+
+
+from ..user_account.schemas import (
+    Create_User_Account_Schema, Update_User_Account_Schema,
+    Get_User_Account_Schema
+)
+
+
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity
+)
 
 # Instance of the account holder repository
 user_account_repository = UserAccountRepository(
@@ -44,6 +48,36 @@ class UserTransactionsApi(CrudApi):
             Get_User_Transaction_Schema,  # Esquema Get para roles
         )
 
+    def handle_inter_wallet_transaction(self, user_id, origin,destination,amount,currency,description):
+        origin = user_account_repository.get_user_account_by_account_number(origin)
+        if not origin or origin==None:
+            return {'status':404, 'message':f'No existe la cuenta de origen: {wallet_origin}'}, 404
+        try:
+            A = user_transactions_repository.create(**{
+                'transaction_type' : 'inter_wallet',
+                'transaction_date' : str(datetime.datetime.now()),
+                'user_id' : user_id,
+                'amount' : amount,
+                'currency_id' : user_transactions_repository.get_currency_by_currency_name(currency).id,
+                'origin_account': origin.id,
+                'destination_account' : 1,
+                'transaction_status_id' : 2,
+                'transaction_description' : description
+            })
+            response = {
+                'status' : 200,
+                'message' : 'Se ha realizado la transferencia Interwallet.',
+                'transaction_id' : A.id,
+            }
+            return response, 200
+        except Exception as e:
+            response = {
+                'status' : 500,
+                'message' : 'La moneda no existe.',
+            }
+            return response, 500
+
+
     @jwt_required(fresh=True)
     def post(self):
         user_identity = get_jwt_identity()
@@ -51,77 +85,95 @@ class UserTransactionsApi(CrudApi):
             user_id = User.decode_token(user_identity)
             data = request.get_json()
 
-            wallet_origin = data.get("origin")
-            wallet_destination = data.get("destination")
-            amount = data.get("amount")
-            trans_type = data.get("transaction_type")
-            currency = data.get("currency")
-            description = data.get("description")
+            wallet_origin = data.get('origin')
+            wallet_destination = data.get('destination')
+            amount = data.get('amount')
+            trans_type = data.get('transaction_type')
+            currency = data.get('currency')
+            description = data.get('description')
             status = 2
 
-            # verificar si el origen y destino son del mismo dueno, de terceros o de un wallet diferente
-            origin = user_account_repository.get_user_account_by_account_number(
-                wallet_origin
-            )
-            if not origin:
-                return {
-                    "code": 404,
-                    "error": f"No existe la cuenta de origen: {wallet_origin}",
-                }, 404
-            destination = user_account_repository.get_user_account_by_account_number(
-                wallet_destination
-            )
-            if not destination:
-                return {
-                    "code": 404,
-                    "error": f"No existe la cuenta de destino: {wallet_destination}:",
+            if trans_type == "inter_wallet":
+                return self.handle_inter_wallet_transaction(user_id, wallet_origin, wallet_destination,amount, currency, description)
+
+            #verificar si el origen y destino existen
+            origin = user_account_repository.get_user_account_by_account_number(wallet_origin)
+            if not origin or origin==None:
+                return {'status':404, 'message':f'No existe la cuenta de origen: {wallet_origin}'}, 404
+            destination = user_account_repository.get_user_account_by_account_number(wallet_destination)
+            if (not destination or destination==None) and trans_type != "inter_wallet":
+                return {'status':404, 'message':f'No existe la cuenta de destino: {wallet_destination}'}, 404
+
+            if origin.user_id == destination.user_id and trans_type != 'b_a':
+                return {'status' : 414, 'message' : 'Para realizar transferencias del mismo usuario, ir a la seccion "Entre Cuentas".'}, 414
+            
+            try:
+                A = user_transactions_repository.create(**{
+                    'transaction_type' : trans_type,
+                    'transaction_date' : str(datetime.datetime.now()),
+                    'user_id' : user_id,
+                    'amount' : amount,
+                    'currency_id' : user_transactions_repository.get_currency_by_currency_name(currency).id,
+                    'origin_account': origin.id,
+                    'destination_account' : destination.id,
+                    'transaction_status_id' : status,
+                    'transaction_description' : description
+                })
+                response = {
+                    'status' : 200,
+                    'message' : 'Se ha realizado la transferencia.',
+                    'transaction_id' : A.id,
                 }
-
-            # new_amount = amount
-            # if user_origin.user_id != user_destination.user_id and trans_type == "Caribbean Wallet": #2%
-            #     new_amount = amount + ((amount*2)/100)
-            # elif user_origin.user_id != user_destination.user_id and trans_type != "Caribbean Wallet": #5%
-            #     new_amount = amount + ((amount*5)/100)
-
-            A = user_transactions_repository.create(
-                **{
-                    "transaction_type": trans_type,
-                    "transaction_date": str(datetime.datetime.now()),
-                    "user_id": user_id,
-                    "amount": amount,
-                    "currency_id": currency,
-                    "origin_account": origin.id,
-                    "destination_account": destination.id,
-                    "transaction_status_id": status,
-                    "transaction_description": description,
+                return response, 200
+            except Exception as e:
+                response = {
+                    'status' : 500,
+                    'message' : 'La moneda no existe.',
                 }
-            )
-            response = {
-                "status": 200,
-                "message": "Se ha realizado la transferencia.",
-                "transaction_id": A.id,
-            }
-            return response, 200
-
+                return response, 500
         else:
-            response = {"status": 401, "message": "No se ha iniciado sesión."}
+            response = { "status" : 401, "message": "No se ha iniciado sesión." }
             return response, 401
 
     @jwt_required(fresh=True)
-    def put(self, id):
+    def put(self,id):
         user_identity = get_jwt_identity()
         if user_identity:
             user_id = User.decode_token(user_identity)
-            A = user_transactions_repository.update(id, **{"transaction_status_id": 3})
-            print(A)
-            response = {
-                "status": 200,
-                "message": "Se ha cancelado la transferencia.",
-                "id": A.id,
-            }
-            return response, 200
+            A = user_transactions_repository.update(id, **{'transaction_status_id': 3})
+            if user_id == A.user_id: 
+                try:
+                    B = user_transactions_repository.create(**{
+                        'transaction_type' : A.transaction_type,
+                        'transaction_date' : str(datetime.datetime.now()),
+                        'user_id' : user_id,
+                        'amount' : A.amount,
+                        'currency_id' : A.currency_id,
+                        'origin_account': A.destination_account,
+                        'destination_account' : A.origin_account,
+                        'transaction_status_id' : A.transaction_status_id,
+                        'transaction_description' : A.transaction_description
+                    })
+                    response = {
+                        'status' : 200,
+                        'message' : 'Se ha cancelado la transferencia.',
+                        'id' : B.id
+                    }
+                    return response, 200
+                except ValidationError as inst:
+                    response = {
+                        'status' : 500,
+                        'message' : list(inst.messages.values())[0][0]
+                    }
+                    return response, 500
+            else:
+                response = {
+                        'status' : 400,
+                        'message' : "La transferencia solo puede ser cancelada por el usuario que la realizó."
+                    }
+                return response, 400
         else:
-            response = {"status": 401, "message": "No se ha iniciado sesión."}
+            response = { "status" : 401, "message": "No se ha iniciado sesión." }
             return response, 401
 
     @jwt_required(fresh=True)
@@ -131,34 +183,15 @@ class UserTransactionsApi(CrudApi):
             user_id = User.decode_token(user_identity)
             A = user_transactions_repository.get_transactions_by_user_id(user_id)
             if len(A) == 0:
-                response = {
-                    "status": 400,
-                    "message": "No se ha realizado ninguna transferencia.",
-                }
+                response = { "status" : 400, "message": "No se ha realizado ninguna transferencia." }
                 return response, 400
             else:
-                B = list(
-                    map(
-                        lambda x: {
-                            "origin": x.origin_account,
-                            "destination": x.destination_account,
-                            "amount": x.amount,
-                            "transaction_type": x.transaction_type,
-                            "transaction_date": x.transaction_date.strftime(
-                                "%m/%d/%Y, %H:%M:%S"
-                            ),
-                            "currency": user_transactions_repository.get_currency_by_currency_id(
-                                x.currency_id
-                            ).name,
-                            "description": x.transaction_description,
-                            "id": x.id,
-                            "status": x.transaction_status_id,
-                        },
-                        A,
-                    )
-                )
-                response = {"status": 200, "transactions": B}
+                B = list(map(lambda x: {"origin" : x.origin_account, "destination" : x.destination_account, "amount" : x.amount,
+                                        "transaction_type" : x.transaction_type, "transaction_date" : x.transaction_date.strftime("%m/%d/%Y, %H:%M:%S"), 
+                                        "currency" : user_transactions_repository.get_currency_by_currency_id(x.currency_id).name,
+                                        "status" : x.transaction_status_id}, A))
+                response = { "status" : 200, "transactions": B }
                 return response, 200
         else:
-            response = {"status": 401, "message": "No se ha iniciado sesión."}
+            response = { "status" : 401, "message": "No se ha iniciado sesión." }
             return response, 401
